007cc720f502ad176342a1fc6db24735805d6cda
zynebb <zynebbouamama@hotmail.fr>
2022-02-28 19:56:40 +0100

    comics

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const cors = require("cors");
 // mongoose.connect(process.env.MONGODB_URI);
 
 const characters = require("./data/characters.json");
-
+const comics = require("./data/comics.json");
 const app = express();
 app.use(cors());
 // console.log(characters);
+console.log(comics);
 app.get("/", (req, res) => {
   res.json({ characters });
+});
+app.get("/comics", (req, res) => {
+  res.json({ comics });
+});
+app.all("*", function (req, res) {
+  res.json({ message: "Page not found" });
 });
 app.listen(process.env.PORT || 3200, () => console.log("server started"));
