3d951ece9c8a9ef873c729d66110435b0f8aa45a
zynebb <zynebbouamama@hotmail.fr>
2022-03-03 15:37:25 +0100

    message

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ app.get("/comics", async (req, res) => {
     console.log(error);
   }
 });
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      ` https://lereacteur-marvel-api.herokuapp.com/comics/5fc8ba1fdc33470f788f88b3?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 // app.get("/comics", (req, res) => {
 //   res.json({ comics });
 // });
